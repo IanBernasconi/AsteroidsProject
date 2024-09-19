a22ceb971aa3e23f48b63eacc45d51298ee34748
Ian Bernasconi <ibernasconi@itba.edu.ar>
2024-09-19 20:39:33 +0200

    Add infinity space functionality

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
 {
 
     public static int SCORE = 0;
-
+    public static float xBorderLimit, yBorderLimit;
     public float thrustForce = 5f;
     public float rotationSpeed = 120f;
 
@@ -15,26 +15,58 @@ public class Player : MonoBehaviour
 
     private Rigidbody _rigid;
 
-    public float fireRate = 0.3f;
+    public float fireRate = 0.4f;
     private float nextFireTime = 0f;
+
+    Vector2 thrustDirection;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigid = GetComponent<Rigidbody>();
+        xBorderLimit = Camera.main.orthographicSize + 1;
+        yBorderLimit = (Camera.main.orthographicSize + 1) * Screen.width / Screen.height;
+    }
+
+    private void FixedUpdate()
+    {
+        float rotation = Input.GetAxis("Rotate") * rotationSpeed * Time.deltaTime;
+        float thrust = Input.GetAxis("Thrust") * thrustForce * Time.deltaTime;
+        thrustDirection = transform.right;
+
+        transform.Rotate(Vector3.forward, -rotation);
+
+        _rigid.AddForce(thrustDirection * thrust);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float rotation = Input.GetAxis("Rotate") * Time.deltaTime;
-        float thrust = Input.GetAxis("Thrust") * Time.deltaTime;
+        var newPos = transform.position;
+        if (newPos.x > xBorderLimit)
+        {
+            newPos.x = -xBorderLimit + 1;
+        }
+        else if (newPos.x < -xBorderLimit)
+        {
+            newPos.x = xBorderLimit - 1;
+        }
+        else if (newPos.y > yBorderLimit)
+        {
+            newPos.y = -yBorderLimit + 1;
+        }
+        else if (newPos.y < -yBorderLimit)
+        {
+            newPos.y = yBorderLimit - 1;
+        }
+        transform.position = newPos;
 
-        Vector3 thrustDirection = transform.right;
 
-        _rigid.AddForce(thrust * thrustForce * thrustDirection);
+        // Vector3 thrustDirection = transform.right;
 
-        transform.Rotate(Vector3.forward, -rotation * rotationSpeed);
+        // _rigid.AddForce(thrust * thrustForce * thrustDirection);
+
+        // transform.Rotate(Vector3.forward, -rotation * rotationSpeed);
 
         if (Input.GetKey(KeyCode.Space) && Time.time >= nextFireTime)
         {
